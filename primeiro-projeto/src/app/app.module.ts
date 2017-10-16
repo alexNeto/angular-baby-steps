05a92663122d60185d14752084fbb9fd0590cb58
alexNeto <alex.neto3@hotmail.com>
2017-10-16 17:37:42 -0200

    terminado aula 1

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiro2Component
   ],
   imports: [
     BrowserModule
